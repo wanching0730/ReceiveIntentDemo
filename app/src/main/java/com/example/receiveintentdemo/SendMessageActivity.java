9065c18169147554fde5f2e02ecf2f7f667c34d7
Wan Ching <wanching_97@hotmail.com>
2017-06-29 14:03:07 +0800

    git add

---
+++ app/src/main/java/com/example/receiveintentdemo/SendMessageActivity.java
@@ -0,0 +1,40 @@
+package com.example.receiveintentdemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+
+public class SendMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_message);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Intent intent = getIntent();
+
+        //receive text message from other activity
+        //add intent-filter in manifest
+        String message = intent.getStringExtra(Intent.EXTRA_TEXT);
+
+        EditText etMessage = (EditText)findViewById(R.id.message);
+        etMessage.setText(message);
+    }
+
+}
